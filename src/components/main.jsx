ad53e5dd0b69551af7a39d498e674b0fe9709237
tyan <tyan@linkedin.com>
2016-11-09 15:21:06 -0800

    Implementing Fetch - lecture 46

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,9 @@
 var React = require('react');
 var Header = require('./header');
 
-var renderCount = 0;
 
 module.exports = React.createClass({
   render: function() {
-    console.log(renderCount++);
 
     return <div>
       <Header />
---
+++ src/utils/api.jsx
@@ -0,0 +1,13 @@
+var Fetch = require('wahtwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = '2fb22689e800cae';
+
+module.exports = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID ' + apiKey
+      }
+    });
+  }
+}
