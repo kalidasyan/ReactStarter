64b08445451292fa5470b1e634da8dd70bdf9773
tyan <tyan@linkedin.com>
2016-11-08 17:50:54 -0800

    Rounter Demo - lecture 42 history@1.17.0

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,38 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
 
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      {this.props.children}
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
-ReactDOM.render(element, document.querySelector('.container'));
+var Child1 = React.createClass({
+  render: function() {
+    return <h1>I'm child1</h1>
+  }
+});
+
+var Child2 = React.createClass({
+  render: function() {
+    return <h1>I'm the other child, child2</h1>
+  }
+});
+
+var routes = (
+  <Router>
+    <Route path="/" component={Hello}>
+      <Route path="1" component={Child1} />
+      <Route path="2" component={Child2} />
+    </Route>
+  </Router>
+);
+
+
+// var element = React.createElement(Hello, {});
+ReactDOM.render(routes, document.querySelector('.container'));
