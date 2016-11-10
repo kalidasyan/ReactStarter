ccd567babac80b94b0699d4533b7f24f1004bdea
tyan <tyan@linkedin.com>
2016-11-09 16:37:53 -0800

    Fetching data naively - lecture 49

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -14,6 +14,7 @@ module.exports = React.createClass({
     if(this.props.children) {
       return this.props.children
     } else {
+      console.log('TopicList');
       return <TopicList />
     }
   }
--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,9 +1,33 @@
 var React = require('react');
+var Api = require('../utils/api');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function() {
+    Api.get('topics/defaults')
+      .then(function(data){
+        console.log(data);
+        this.setState({
+          topics: data.data
+        })
+      }.bind(this));
+  },
   render: function() {
     return <div className="list-group">
       Topic List
+      {this.renderTopics()}
     </div>
+  },
+  renderTopics: function() {
+    console.log(this.state.topics);
+    return this.state.topics.map(function( topic ){
+      return <li>
+        {topic.description}
+      </li>
+    });
   }
 });
